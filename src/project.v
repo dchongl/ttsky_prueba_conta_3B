e28594d8b3d9decdab6028b761dba30e7334d790
dchongl <dchongl1700@gmail.com>
2026-01-06 13:22:21 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -39,7 +39,7 @@ module tt_um_conta (
 
     
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
     assign uio_out [2:0] = 3'b000;
   assign uio_oe  = 0;
 
